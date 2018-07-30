4f90a9c28f9cdbd6825a98a54d97bc3d741541d5
Al <faried_94@hotmail.com>
2018-07-31 01:05:03 +0800

    Added additional reminder for task reminders

--- flow_reminder_bot.py
+++ flow_reminder_bot.py
@@ -7,16 +7,22 @@ TOKEN = '661877002:AAGwwd3zN0ZLObDAObV6FqRGCQkmphEkrus'             # @SMUCLEBot
 REMINDER_TOKEN = '611195322:AAFtHWQpwJBGW9cHsSzkWo99Gu5d4UmHVFM'    # @SMUCLEReminderBot
 FYP_CHAT_ID = '-1001296496624'
 
-utcTime = datetime.datetime.time(datetime.datetime.utcnow()).strftime("%H:%M:%S")
+utcDateTime = datetime.datetime.utcnow()
+utcDay = datetime.datetime.date(utcDateTime).strftime("%A")
+utcTime = datetime.datetime.time(utcDateTime).strftime("%H:%M:%S")
 
 bot = telegram.Bot(token=REMINDER_TOKEN)
 
-if utcTime.split(':')[0] == '01':
-    bot.send_message(chat_id=FYP_CHAT_ID, text='Hi, this is an automated reminder. Please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
-elif utcTime.split(':')[0] == '04':
-    bot.send_message(chat_id=FYP_CHAT_ID, text='This is another automated reminder, please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
-elif utcTime.split(':')[0] == '10':
-    bot.send_message(chat_id=FYP_CHAT_ID, text='Final reminder for those who have not done so, please complete your workflow survery. Thanks!\n\nWorkflow Survey: https://smu.sg/flow')
+if utcDay == 'Saturday'
+    if utcTime.split(':')[0] == '01':
+        bot.send_message(chat_id=FYP_CHAT_ID, text='Hi, this is an automated reminder. Please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
+    elif utcTime.split(':')[0] == '04':
+        bot.send_message(chat_id=FYP_CHAT_ID, text='This is another automated reminder, please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
+    elif utcTime.split(':')[0] == '10':
+        bot.send_message(chat_id=FYP_CHAT_ID, text='Final reminder for those who have not done so, please complete your workflow survery. Thanks!\n\nWorkflow Survey: https://smu.sg/flow')
+
+bot.send_message(chat_id=FYP_CHAT_ID, text='Please remember to update group Monkey Business HQ on the:\n\n1) Status of your task yesterday\n2) Task you\'re working on today\n3) And if you require any help with any task.\n\nThanks! :)')
+
 
 # SIDE PROJECT:
 # Program bot so that it can take in requests to set reminder.
