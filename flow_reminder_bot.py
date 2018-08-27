3ecf2a4786f5d12b38918bfe632aa1ef9532d9cf
Al <faried_94@hotmail.com>
2018-08-27 18:22:54 +0800

    Update telegram bot reminder

--- flow_reminder_bot.py
+++ flow_reminder_bot.py
@@ -6,6 +6,7 @@ from telegram.ext import CommandHandler
 TOKEN = '661877002:AAGwwd3zN0ZLObDAObV6FqRGCQkmphEkrus'             # @SMUCLEBot
 REMINDER_TOKEN = '611195322:AAFtHWQpwJBGW9cHsSzkWo99Gu5d4UmHVFM'    # @SMUCLEReminderBot
 FYP_CHAT_ID = '-1001296496624'
+DA_CHAT_ID = '-284543929'
 
 utcDateTime = datetime.datetime.utcnow()
 utcDay = datetime.datetime.date(utcDateTime).strftime("%A")
@@ -20,6 +21,16 @@ if utcDay == 'Saturday':
         bot.send_message(chat_id=FYP_CHAT_ID, text='This is another automated reminder, please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
     elif utcTime.split(':')[0] == '10':
         bot.send_message(chat_id=FYP_CHAT_ID, text='Final reminder for those who have not done so, please complete your workflow survery. Thanks!\n\nWorkflow Survey: https://smu.sg/flow')
+
+if utcDay == 'Thursday':
+    if utcTime.split(':')[0] == '13':
+        bot.send_message(chat_id=DA_CHAT_ID, text='Remember to do DA self check quiz. Due date is 2300 hrs tomorrow!')
+
+if utcDay == 'Friday':
+    if utcTime.split(':')[0] == '12':
+        bot.send_message(chat_id=DA_CHAT_ID, text='Remember to do DA self check quiz. Due date is 2300 hrs today!')
+    elif utcTime.split(':')[0] == '14':
+        bot.send_message(chat_id=DA_CHAT_ID, text='Final reminder, do DA self check quiz. Due date is 2300 hrs today!')
 
 if utcTime.split(':')[0] == '01':
     bot.send_message(chat_id=FYP_CHAT_ID, text='Please remember to update group Monkey Business HQ on the:\n\n1) Status of your task yesterday\n2) Task you\'re working on today\n3) And if you require any help with any task.\n\nThanks!')
