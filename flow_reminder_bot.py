c87324db1994d3b5052d1410cef40d74486185c5
Al <faried_94@hotmail.com>
2018-08-29 16:04:38 +0800

    Update messages

--- flow_reminder_bot.py
+++ flow_reminder_bot.py
@@ -7,6 +7,7 @@ TOKEN = '661877002:AAGwwd3zN0ZLObDAObV6FqRGCQkmphEkrus'             # @SMUCLEBot
 REMINDER_TOKEN = '611195322:AAFtHWQpwJBGW9cHsSzkWo99Gu5d4UmHVFM'    # @SMUCLEReminderBot
 FYP_CHAT_ID = '-1001296496624'
 DA_CHAT_ID = '-284543929'
+SELF_CHAT_ID = '240466360'
 
 utcDateTime = datetime.datetime.utcnow()
 utcDay = datetime.datetime.date(utcDateTime).strftime("%A")
@@ -15,26 +16,34 @@ utcTime = datetime.datetime.time(utcDateTime).strftime("%H:%M:%S")
 bot = telegram.Bot(token=REMINDER_TOKEN)
 
 if utcDay == 'Saturday':
-    if utcTime.split(':')[0] == '01':
-        bot.send_message(chat_id=FYP_CHAT_ID, text='Hi, this is an automated reminder. Please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
-    elif utcTime.split(':')[0] == '04':
-        bot.send_message(chat_id=FYP_CHAT_ID, text='This is another automated reminder, please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
+    if utcTime.split(':')[0] == '04':
+        bot.send_message(chat_id=FYP_CHAT_ID, text='Hi, please complete your flow survey.\n\nWorkflow Survey: https://smu.sg/flow')
     elif utcTime.split(':')[0] == '10':
         bot.send_message(chat_id=FYP_CHAT_ID, text='Final reminder for those who have not done so, please complete your workflow survery. Thanks!\n\nWorkflow Survey: https://smu.sg/flow')
 
 if utcDay == 'Thursday':
     if utcTime.split(':')[0] == '13':
-        bot.send_message(chat_id=DA_CHAT_ID, text='Remember to do DA self check quiz. Due date is 2300 hrs tomorrow!')
+        bot.send_message(chat_id=DA_CHAT_ID, text='Remember to do DA self check quiz on Elearn. Due date is 2300 hrs tomorrow!\n\nElearn link:\nhttps://elearn.smu.edu.sg')
 
 if utcDay == 'Friday':
-    if utcTime.split(':')[0] == '12':
-        bot.send_message(chat_id=DA_CHAT_ID, text='Remember to do DA self check quiz. Due date is 2300 hrs today!')
-    elif utcTime.split(':')[0] == '14':
-        bot.send_message(chat_id=DA_CHAT_ID, text='Final reminder, do DA self check quiz. Due date is 2300 hrs today!')
+    if utcTime.split(':')[0] == '13':
+        bot.send_message(chat_id=DA_CHAT_ID, text='Final reminder for those who have not done so, please do your DA self check quiz on Elearn. Due date is 2300 hrs today!\n\nElearn link:\nhttps://elearn.smu.edu.sg')
 
 if utcTime.split(':')[0] == '01':
-    bot.send_message(chat_id=FYP_CHAT_ID, text='Please remember to update group Monkey Business HQ on the:\n\n1) Status of your task yesterday\n2) Task you\'re working on today\n3) And if you require any help with any task.\n\nGroup Link: https://t.me/joinchat/DlU5uFBQ-YK_fFidHRDu5w\n\nThanks!')
-
+    bot.send_message(
+        chat_id=FYP_CHAT_ID,
+        text='<b>Daily Reminders</b>\n\n' \
+            'Please remember to update group <i>"Monkey Business HQ"</i> on the:\n\n' \
+            '1) Status of your task yesterday\n' \
+            '2) Task you\'re working on today\n' \
+            '3) And if you require any help with any task.\n\n' \
+            'Monkey Business HQ Group Link:\n' \
+            'https://t.me/joinchat/DlU5uFBQ-YK_fFidHRDu5w\n\n' \
+            'Please also check <i>"THUNDERHEAD App updates"</i> channel before running application to get the most up to date configurations.\n\n' \
+            'THUNDERHEAD App updates Group link:\n' \
+            'https://t.me/joinchat/AAAAAEShIIlBDk17NJzbrg',
+        parse_mode=telegram.ParseMode.HTML
+    )
 
 # SIDE PROJECT:
 # Program bot so that it can take in requests to set reminder.
