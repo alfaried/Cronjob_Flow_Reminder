14bd1d3e75a0c461bf3d534e019c64bccf2db71b
Al <faried_94@hotmail.com>
2018-09-02 14:36:56 +0800

    add test file create_user.py

---
+++ create_user.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def addUser(username):
+    bashCommand = 'sudo adduser ' + username
+
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    return output, error
+
+if __name__ == "__main__":
+    output, error = addUser(test)
+    print(output)
