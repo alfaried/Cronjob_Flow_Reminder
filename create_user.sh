ab540b2b569220e10cbf8bf8ff0616c8627c7e62
Al <faried_94@hotmail.com>
2018-09-02 18:00:42 +0800

    *

--- create_user.sh
+++ create_user.sh
@@ -3,13 +3,15 @@ sudo adduser test
 
 # Create folder '.ssh'
 sudo mkdir /home/test/.ssh
-sudo chown test:test /home/test/.ssh
 sudo chmod 700 /home/test/.ssh
 
 # Create file 'authorized_keys'
 sudo touch /home/test/.ssh/authorized_keys
-sudo chown test:test /home/test/.ssh/authorized_keys
 sudo chmod 600 /home/test/.ssh/authorized_keys
 
-# Need to add the public key into the .ssh/authorized_keys file
+# Add the public key into the .ssh/authorized_keys file
 sudo echo '# test public key' >> /home/test/.ssh/authorized_keys
+
+# Assign new user
+sudo chown test:test /home/test/.ssh
+sudo chown test:test /home/test/.ssh/authorized_keys
