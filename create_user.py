a8184fc5bc37185839f760e86d9f6a203ea4761e
Al <faried_94@hotmail.com>
2018-09-02 19:06:45 +0800

    *

--- create_user.py
+++ create_user.py
@@ -18,7 +18,7 @@ def addPublicKey(username=None,public_key=None):
     pk_dir = '/home/' + username + '/.ssh/authorized_keys'
     # bashCommand_1 = 'sudo bash -c "echo \'# ' + username + ' public key\' >> ' + pk_dir + '"'
     # bashCommand_2 = 'sudo bash -c "echo \'' + public_key + '\' >> ' + pk_dir + '"'
-    bashCommand_1 = 'sudo bash -c "\"echo hi >> ' + pk_dir + '"\"'
+    bashCommand_1 = 'sudo bash -c "echo hi >> ' + pk_dir + '"'
     print(bashCommand_1)
     try:
         executeBash(bashCommand_1)
