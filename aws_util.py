4de4b53c4be23c1c7415797bf31f6f0fc1efb3ae
Al <faried_94@hotmail.com>
2018-09-02 23:37:34 +0800

    debug codes

--- aws_util.py
+++ aws_util.py
@@ -20,13 +20,14 @@ def addPublicKey(username=None,public_key=None):
     pk_dir = '/home/' + username + '/.ssh/authorized_keys'
     bashCommand = 'sudo bash -c "echo # ' + username + ' public key >> ' + pk_dir + '"'
     bashCommand_1 = 'sudo bash -c "echo ' + public_key + ' >> ' + pk_dir + '"'
-    print('test')
+    print(username)
+    print(public_key)
     try:
         subprocess.Popen(shlex.split(bashCommand), stdout=subprocess.PIPE)
         subprocess.Popen(shlex.split(bashCommand_1), stdout=subprocess.PIPE)
     except:
         raise Exception('Unable to add public key for user ' + username)
-
+    print('test')
     return True
 
 
