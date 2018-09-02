4091e8869e3353f9e089ae3a748eea58062fe2f4
Al <faried_94@hotmail.com>
2018-09-02 15:06:57 +0800

    test

--- create_user.py
+++ create_user.py
@@ -2,25 +2,25 @@ import os
 import subprocess
 from pathlib import Path
 
-# Return output : byte, error : default-None
+# Return output : tuple, error : default-None
 def executeBash(bashCommand):
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     return process.communicate()
 
 
-# Return output : byte, error : default-None
+# Return output : tuple, error : default-None
 def addUser(username):
     bashCommand = 'sudo adduser ' + username
     return executeBash(bashCommand)
 
 
-# Return output : byte, error : default-None
+# Return output : tuple, error : default-None
 def delUser(username):
     bashCommand = 'sudo userdel -r ' + username
     return executeBash(bashCommand)
 
 
-# Return output : byte, error : default-None
+# Return output : tuple, error : default-None
 def accessUser(username):
     bashCommand = 'sudo su - ' + username
     return executeBash(bashCommand)
@@ -29,8 +29,11 @@ def accessUser(username):
 if __name__ == "__main__":
     # Run test commands here
     # home = str(Path.home())
-    output = addUser('test')
-
-    if output.decode('utf-8') == '':
-        dir = '/home/test/.ssh'
-        os.makedirs(dir)
+    output,error = addUser('test')
+    print(output)
+    print(type(output))
+    print(error)
+    print(type(error))
+    # if output.decode('utf-8') == '':
+    #     dir = '/home/test/.ssh'
+    #     os.makedirs(dir)
