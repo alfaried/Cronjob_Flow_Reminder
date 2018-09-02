7be8d16a30b4a4b4a15d59c3c64c3be53d92ec54
Al <faried_94@hotmail.com>
2018-09-02 14:42:44 +0800

    edit create_user ile

--- create_user.py
+++ create_user.py
@@ -1,13 +1,32 @@
 import subprocess
 
+# Return byte type : output, error
+def executeBash(bashCommand):
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    return process.communicate()
+
+
+# Return byte type : output, error
 def addUser(username):
     bashCommand = 'sudo adduser ' + username
+    return executeBash(bashCommand)
 
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
 
-    return output, error
+# Return byte type : output, error
+def accessUser(username):
+    bashCommand = 'sudo su - ' + username
+    return executeBash(bashCommand)
+
 
 if __name__ == "__main__":
     output, error = addUser('test')
+    print('Creating user..')
     print(output)
+    print(error)
+    print('\n')
+
+    output, error = accessUser('test')
+    print('Accessing user..')
+    print(output)
+    print(error)
+    print('\n')
