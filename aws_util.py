ff0d4c43aed447c821ddbb7ab6e9478889bc3f50
Al <faried_94@hotmail.com>
2018-09-08 00:27:43 +0800

    *

--- aws_util.py
+++ aws_util.py
@@ -1,7 +1,7 @@
 import os
 import boto3
 import shlex, subprocess
-
+from collections import defaultdict
 
 # Return output : tuple, error : default-None
 def executeBash(bashCommand):
@@ -166,13 +166,42 @@ def delAWSKeyPair(username=None):
 
 if __name__ == "__main__":
     # Run test commands here
-    username = 'Test'
+    # username = 'Test'
     # response = generateKeyPair(username)
     # print(response['public_key'])
 
-    response = generateKeyPair(username)
-    public_key = response['public_key']
+    # response = generateKeyPair(username)
+    # public_key = response['public_key']
+    #
+    # delUser(username)
+    # response = addUser(username,public_key)
+    # print(response)
 
-    delUser(username)
-    response = addUser(username,public_key)
-    print(response)
+    # Connect to EC2
+    ec2 = boto3.resource('ec2')
+
+    # Get information for all running instances
+    running_instances = ec2.instances.filter(Filters=[{
+        'Name': 'instance-state-name',
+        'Values': ['running']}])
+
+    ec2info = defaultdict()
+    for instance in running_instances:
+        for tag in instance.tags:
+            if 'Name'in tag['Key']:
+                name = tag['Value']
+        # Add instance info to a dictionary
+        ec2info[instance.id] = {
+            'Name': name,
+            'Type': instance.instance_type,
+            'State': instance.state['Name'],
+            'Private IP': instance.private_ip_address,
+            'Public IP': instance.public_ip_address,
+            'Launch Time': instance.launch_time
+            }
+
+    attributes = ['Name', 'Type', 'State', 'Private IP', 'Public IP', 'Launch Time']
+    for instance_id, instance in ec2info.items():
+        for key in attributes:
+            print("{0}: {1}".format(key, instance[key]))
+        print("------")
